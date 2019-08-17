c47f8852b2e188c58e21f0a6b1933ac36efa6b94
MYPC <MYPC@HP>
2019-08-17 12:59:54 +0530

    1st file change

---
+++ src/main/java/com/app/Student.java
@@ -0,0 +1,48 @@
+package com.app;
+
+public class Student {
+	private int studentId;
+	private String studentName;
+
+	/**
+	 * @return the studentId
+	 */
+	public int getStudentId() {
+		return studentId;
+	}
+
+	/**
+	 * @param studentId the studentId to set
+	 */
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	/**
+	 * @return the studentName
+	 */
+	public String getStudentName() {
+		return studentName;
+	}
+
+	/**
+	 * @param studentName the studentName to set
+	 */
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", studentName=" + studentName + "]";
+	}
+
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.setStudentId(12);
+		s.setStudentName("Niranjan");
+		System.out.println(s);
+
+	}
+
+}
